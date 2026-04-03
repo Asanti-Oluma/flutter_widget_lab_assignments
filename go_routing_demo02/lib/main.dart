bb8f0a560af6898041107c7a0f884f7fce04d4b3
Asanti oluma <olumaasanti143@gmail.com>
2026-04-03 18:11:24 +0300

    Added projects with gitignore

---
+++ go_routing_demo02/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() => runApp(const MyApp());
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(
+      path: '/product/:id',
+      builder: (context, state) {
+        final id = state.pathParameters['id']!;
+        final filter = state.uri.queryParameters['filter'] ?? 'none';
+        return ProductDetailScreen(id: id, filter: filter);
+      },
+    ),
+  ],
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'go_router Lesson 2',
+      routerConfig: _router,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home'), centerTitle: false),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/product/42?filter=popular'),
+          child: const Text('Go to Product Details'),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductDetailScreen extends StatelessWidget {
+  final String id;
+  final String filter;
+
+  const ProductDetailScreen({
+    super.key,
+    required this.id,
+    required this.filter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Product $id'), centerTitle: false),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('Showing product $id'), Text('Filter: $filter')],
+        ),
+      ),
+    );
+  }
+}
